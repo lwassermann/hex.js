c0b961d5b588ae739842ad2f7361e723d204765e
Lars Wassermann <lwassermann@babbel.com>
2015-06-04 23:35:18 +0200

    refactored Maybe to have an explicit Nothing

--- src/contexts.js
+++ src/contexts.js
@@ -2,13 +2,16 @@ import R from 'ramda';
 
 const MaybeProto = {
   map: R.curry(function(fn) {
-    if (!R.isNil(this.value)) { return Maybe(fn(this.value)); }
-
-    return this;
+    return Maybe(fn(this.value));
   })
 };
 
+const Nothing = {};
+Nothing.map = R.always(Nothing);
+
 const Maybe = function(c) {
+  if (R.isNil(c)) { return Nothing; }
+
   const newMaybe = Object.create(MaybeProto);
   newMaybe.value = c;
   return newMaybe;
